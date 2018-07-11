5a3cf7a71db514293fb3c51c39fd684f5d9ddea4
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-07-11 11:15:04 +0300

    Update SerialLog.h

--- src/SerialLog.h
+++ src/SerialLog.h
@@ -47,7 +47,7 @@
 
 #ifdef DEBUG
   #if ( (DEBUG != LV_NONE) && (DEBUG != LV_LOW) && (DEBUG != LV_NORMAL) && (DEBUG != LV_HIGH) )
-	#error "DEBUG must be in LV_LOW or LV_NORMAL or LV_HIGH or LV_NONE!"
+  #error "DEBUG must be in LV_LOW or LV_NORMAL or LV_HIGH or LV_NONE!"
   #elif (DEBUG != LV_NONE)
     #warning "Comment #define DEBUG for disable debug messages and free memory!"
   #endif
@@ -60,96 +60,96 @@
 
 #ifdef DEBUG
   #include "Arduino.h"
-	#include "Printable.h"
+  #include "Printable.h"
 
-	#ifdef DEBUG_MEMORY
-	  #include "MemoryFree.h"
-	#endif
+  #ifdef DEBUG_MEMORY
+    #include "MemoryFree.h"
+  #endif
 
-	//macros set log level
-	#if DEBUG == LV_LOW
-		#define LOG(logMessage, ...)       graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_LN(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-		#define LOG_L(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_L_LN(logMessage, ...)  graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-		#define LOG_N(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_N_LN(logMessage, ...)  graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-		#define LOG_H(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_H_LN(logMessage, ...)  graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-	#elif DEBUG == LV_NORMAL
-		#define LOG(logMessage, ...)
-		#define LOG_LN(logMessage, ...)
-		#define LOG_L(logMessage, ...)
-		#define LOG_L_LN(logMessage, ...)
-		#define LOG_N(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_N_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-		#define LOG_H(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_H_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true,  ##__VA_ARGS__)
-	#elif DEBUG==LV_HIGH
-		#define LOG(logMessage, ...)
-		#define LOG_LN(logMessage, ...)
-		#define LOG_L(logMessage, ...)
-		#define LOG_L_LN(logMessage, ...)
-		#define LOG_N(logMessage, ...)
-		#define LOG_N_LN(logMessage, ...)
-		#define LOG_H(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
-		#define LOG_H_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
-	#endif
+  //macros set log level
+  #if DEBUG == LV_LOW
+    #define LOG(logMessage, ...)       graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_LN(logMessage, ...)   graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+    #define LOG_L(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_L_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+    #define LOG_N(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_N_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+    #define LOG_H(logMessage, ...)     graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_H_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+  #elif DEBUG == LV_NORMAL
+    #define LOG(logMessage, ...)
+    #define LOG_LN(logMessage, ...)
+    #define LOG_L(logMessage, ...)
+    #define LOG_L_LN(logMessage, ...)
+    #define LOG_N(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_N_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+    #define LOG_H(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_H_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+  #elif DEBUG==LV_HIGH
+    #define LOG(logMessage, ...)
+    #define LOG_LN(logMessage, ...)
+    #define LOG_L(logMessage, ...)
+    #define LOG_L_LN(logMessage, ...)
+    #define LOG_N(logMessage, ...)
+    #define LOG_N_LN(logMessage, ...)
+    #define LOG_H(logMessage, ...)    graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, false, ##__VA_ARGS__)
+    #define LOG_H_LN(logMessage, ...) graf::SerialLog::LogPrint(logMessage, __FILENAME__, __LINE__, true, ##__VA_ARGS__)
+  #endif
 
-	//macros begin, end
-	#define LOG_BEGIN(baudrate) graf::SerialLog::Begin(baudrate)
-	#define LOG_END() graf::SerialLog::End()
+  //macros begin, end
+  #define LOG_BEGIN(baudrate) graf::SerialLog::Begin(baudrate)
+  #define LOG_END() graf::SerialLog::End()
 
-	//macros get only file name without path
-	#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+  //macros get only file name without path
+  #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 
-	namespace graf {
+  namespace graf {
 
-	class SerialLog {
-	public:
-		static const char* getVersion();
+  class SerialLog {
+  public:
+    static const char* getVersion();
 
-		static void Begin(unsigned long baud);
-		static void End();
+    static void Begin(unsigned long baud);
+    static void End();
 
-		static void LogPrint(int logMessage, 			                  const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
-		static void LogPrint(unsigned int logMessage, 	            const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
-		static void LogPrint(char logMessage,                       const char* cFileName, const int iLine, const bool lineEnd);
-		static void LogPrint(unsigned char logMessage, 	            const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
-		static void LogPrint(const char logMessage[],               const char* cFileName, const int iLine, const bool lineEnd);
-		static void LogPrint(const Printable& logMessage,           const char* cFileName, const int iLine, const bool lineEnd);
-		static void LogPrint(const String& logMessage,              const char* cFileName, const int iLine, const bool lineEnd);
-		static void LogPrint(long logMessage,              			    const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
-		static void LogPrint(unsigned long logMessage,              const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
-		static void LogPrint(double logMessage,                     const char* cFileName, const int iLine, const bool lineEnd, int format= 2);
-		static void LogPrint(const __FlashStringHelper* logMessage, const char* cFileName, const int iLine, const bool lineEnd);
+    static void LogPrint(int logMessage,                        const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
+    static void LogPrint(unsigned int logMessage,               const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
+    static void LogPrint(char logMessage,                       const char* cFileName, const int iLine, const bool lineEnd);
+    static void LogPrint(unsigned char logMessage,              const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
+    static void LogPrint(const char logMessage[],               const char* cFileName, const int iLine, const bool lineEnd);
+    static void LogPrint(const Printable& logMessage,           const char* cFileName, const int iLine, const bool lineEnd);
+    static void LogPrint(const String& logMessage,              const char* cFileName, const int iLine, const bool lineEnd);
+    static void LogPrint(long logMessage,                       const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
+    static void LogPrint(unsigned long logMessage,              const char* cFileName, const int iLine, const bool lineEnd, int format = DEC);
+    static void LogPrint(double logMessage,                     const char* cFileName, const int iLine, const bool lineEnd, int format= 2);
+    static void LogPrint(const __FlashStringHelper* logMessage, const char* cFileName, const int iLine, const bool lineEnd);
 
-	private:
-		static unsigned long baud_;
-		static bool isInit_;
-		static bool isHeadPrint_;
-		static void Initialization();
-		static void Finalization();
-		static void LogFormat(const char* cFileName, const int iLine, const bool lineEnd);
-	};
+  private:
+    static unsigned long baud_;
+    static bool isInit_;
+    static bool isHeadPrint_;
+    static void Initialization();
+    static void Finalization();
+    static void LogFormat(const char* cFileName, const int iLine, const bool lineEnd);
+  };
 
 } /* namespace graf */
 
 #else //not DEBUG
 
-	//macros begin, end
-	#define LOG_BEGIN(baudrate)
-	#define LOG_END()
+  //macros begin, end
+  #define LOG_BEGIN(baudrate)
+  #define LOG_END()
 
-	#define LOG(logMessage, ...)
-	#define LOG_LN(logMessage, ...)
-	#define LOG_L(logMessage, ...)
-	#define LOG_L_LN(logMessage, ...)
-	#define LOG_N(logMessage, ...)
-	#define LOG_N_LN(logMessage, ...)
-	#define LOG_H(logMessage, ...)
-	#define LOG_H_LN(logMessage, ...)
+  #define LOG(logMessage, ...)
+  #define LOG_LN(logMessage, ...)
+  #define LOG_L(logMessage, ...)
+  #define LOG_L_LN(logMessage, ...)
+  #define LOG_N(logMessage, ...)
+  #define LOG_N_LN(logMessage, ...)
+  #define LOG_H(logMessage, ...)
+  #define LOG_H_LN(logMessage, ...)
 
 
 #endif
